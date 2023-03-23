5dd145f27f1471bddd6e3fe3bb983f1f63745dd5
Mikaël Popowicz <mikael.popowicz@gmail.com>
2023-03-23 11:07:20 +0000

    init: initial commit

---
+++ src/CategoryHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oneduo\LighthouseCategoryHandler;
+
+use GraphQL\Error\Error;
+use Nuwave\Lighthouse\Exceptions\AuthenticationException;
+use Nuwave\Lighthouse\Exceptions\AuthorizationException;
+use Nuwave\Lighthouse\Exceptions\RateLimitException;
+use Nuwave\Lighthouse\Exceptions\ValidationException;
+use Nuwave\Lighthouse\Execution\ErrorHandler;
+
+final class CategoryHandler implements ErrorHandler
+{
+    public function __invoke(?Error $error, \Closure $next): ?array
+    {
+        if ($error === null) {
+            return $next(null);
+        }
+
+        $underlyingException = $error->getPrevious();
+
+        $category = match($underlyingException::class) {
+            AuthenticationException::class => 'authentication',
+            AuthorizationException::class => 'authorization',
+            RateLimitException::class => 'rate-limit',
+            ValidationException::class => 'validation',
+            default => 'graphql',
+        };
+
+        $newError = new Error(
+            $error->getMessage(),
+            $error->getNodes(),
+            $error->getSource(),
+            $error->getPositions(),
+            $error->getPath(),
+            $error->getPrevious(),
+            [
+                'category' => $category,
+                ...($error->getExtensions() ?? []),
+            ],
+        );
+
+        return $next($newError);
+    }
+}
